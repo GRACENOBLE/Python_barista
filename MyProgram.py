bf2187d89da47af9693c5eaa594ffd8bf42d7f80
GraceNoble <gracenoble72@gmail.com>
2023-10-11 12:37:10 +0300

    Adding simple maths to the robot barista to allow him take input from the user and generate a bill basing on the order chosen by the customer

--- MyProgram.py
+++ MyProgram.py
@@ -1,14 +1,28 @@
 name = input("\n\nHello, what is your name?\n")
 
-choice =int (input("Hello, welcome to the coffee shop, may i take your order?\t\t PLEASE SELECT A NUMBER FROM THE LIST BELOW\n1.Coffee------8$\n2.Cappuccino-------9$\n3.Fresco------10\n4.Coktail-----11\n\n"))
+choice =int (input("\n\nHello " + name + ", welcome to the coffee shop, may i take your order?\nPLEASE SELECT A NUMBER FROM THE LIST BELOW;\n1.Coffee-------8$\n2.Cappuccino---9$\n3.Fresco------10$\n4.Coktail-----11&\n\n"))
+
+COFFEE = 8
+
+CAPPUCCINO = 9
+
+FRESCO = 10
+
+COKTAIL = 11
+
+number = int (input("\nHow many of those will you take?\n"))
 
 if choice == 1:
-    print("Okay " + name + " your Coffe will be ready in about five minutes.")
+    ctotal = COFFEE * number
+    print("\nOkay " + str(name) + " your Coffe will be ready in about five minutes." + "\nThat'll be " + str(ctotal) + "$\n")
 elif choice == 2:
-    print("Okay " + name + " your Cappuccino will be ready in about five minutes.")
+    cptotal = CAPPUCCINO * number
+    print("\nOkay " + str(name) + " your Cappuccino will be ready in about five minutes." + "\nThat'll be " + str(cptotal) + "$\n")
 elif choice == 3:
-    print("Okay " + name + " your Fresco will be ready in about five minutes.")
+    ftotal = FRESCO * number
+    print("\nOkay " + str(name) + " your Fresco will be ready in about five minutes." + "\nThat'll be " + str(ftotal) + "$\n")
 elif choice == 4:
-    print("Okay " + name + " your Coktail will be ready in about five minutes.")
+    cototal = COKTAIL * number
+    print("\nOkay " + str(name) + " your Coktail will be ready in about five minutes." + "\nThat'll be " + str(cototal) + "$\n")
 else:
-    print("We dont have that yet.")
+    print("\nWe dont have that yet.\n")
