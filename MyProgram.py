e26bac63df5f690562cc1eca9eb74e633f5a2b2f
GraceNoble <gracenoble72@gmail.com>
2023-10-15 07:09:41 +0300

    Adding comments to code and changing ampersand symbol to dollar sign on menu

--- MyProgram.py
+++ MyProgram.py
@@ -1,11 +1,11 @@
-name = input("\n\nHello, what is your name?\n")
+name = input("\n\nHello, what is your name?\n")#asking user their name.
 
-print("\n\nHello " + name + ", welcome to the coffee shop, this is our menu\n1.Coffee-------8$\n2.Cappuccino---9$\n3.Fresco------10$\n4.Coktail-----11&\n\n")
+print("\n\nHello " + name + ", welcome to the coffee shop, this is our menu\n1.Coffee-------8$\n2.Cappuccino---9$\n3.Fresco------10$\n4.Coktail-----11&\n\n")#showing them the menu.
 
-orders = int(input("How many orders would you like to make?\n"))
+orders = int(input("How many orders would you like to make?\n"))#asking the number of orders.
 
-def one_order():
-    one_order = int(input("\nWhat would you like to have? Please select one item.\n1.Coffee-------8$\n2.Cappuccino---9$\n3.Fresco------10$\n4.Coktail-----11&\n"))
+def one_order():#function that handles the case that the user makes one order.
+    one_order = int(input("\nWhat would you like to have? Please select one item.\n1.Coffee-------8$\n2.Cappuccino---9$\n3.Fresco------10$\n4.Coktail-----11$\n"))
 
     number = int (input("\nHow many of those will you take?\n"))
 
@@ -33,19 +33,19 @@ def one_order():
         print("\nWe dont have that yet.\n")
     exit()
 
-def two_orders():
+def two_orders():#function that handles the case that the user makes 2 orders.
     print("comming soon")
     exit()
 
-def three_orders():
+def three_orders():#function that handles the case that the user makes 3 orders.
     print("comming soon")
     exit()
 
-def four_orders():
+def four_orders():#function that handles the case that the user makes 4 orders.
     print("comming soon")
     exit()
 
-if orders == 1:
+if orders == 1:#Calling respective functions basing on the number of orders made.
     one_order()
 elif orders == 2:
     two_orders()
@@ -56,23 +56,3 @@ elif orders == 4:
 else:
     ptint("Sorry we can only accept a maximum of four orders at a time")
     exit()
-
-
-"""
-
-number = int (input("\nHow many of those will you take?\n"))
-
-if choice == 1:
-    ctotal = COFFEE * number
-    print("\nOkay " + str(name) + " your Coffe will be ready in about five minutes." + "\nThat'll be " + str(ctotal) + "$\n")
-elif choice == 2:
-    cptotal = CAPPUCCINO * number
-    print("\nOkay " + str(name) + " your Cappuccino will be ready in about five minutes." + "\nThat'll be " + str(cptotal) + "$\n")
-elif choice == 3:
-    ftotal = FRESCO * number
-    print("\nOkay " + str(name) + " your Fresco will be ready in about five minutes." + "\nThat'll be " + str(ftotal) + "$\n")
-elif choice == 4:
-    cototal = COKTAIL * number
-    print("\nOkay " + str(name) + " your Coktail will be ready in about five minutes." + "\nThat'll be " + str(cototal) + "$\n")
-else:
-    print("\nWe dont have that yet.\n")"""
