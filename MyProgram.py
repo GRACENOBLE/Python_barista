c38b64c1ec103d4e9420b9793e54bfaf4b0289e2
GraceNoble <gracenoble72@gmail.com>
2023-10-15 06:56:53 +0300

    adding the ability to choose more than one order, integration of one order at this point, 2,3 & 4 not yet implemented.

--- MyProgram.py
+++ MyProgram.py
@@ -5,7 +5,32 @@ print("\n\nHello " + name + ", welcome to the coffee shop, this is our menu\n1.C
 orders = int(input("How many orders would you like to make?\n"))
 
 def one_order():
-    print("comming soon")
+    one_order = int(input("\nWhat would you like to have? Please select one item.\n1.Coffee-------8$\n2.Cappuccino---9$\n3.Fresco------10$\n4.Coktail-----11&\n"))
+
+    number = int (input("\nHow many of those will you take?\n"))
+
+    COFFEE = 8
+
+    CAPPUCCINO = 9
+
+    FRESCO = 10
+
+    COKTAIL = 11
+
+    if one_order == 1:
+        ctotal = COFFEE * number
+        print("\nOkay " + str(name) + " your Coffe will be ready in about five minutes." + "\nThat'll be " + str(ctotal) + "$\n")
+    elif one_order == 2:
+        cptotal = CAPPUCCINO * number
+        print("\nOkay " + str(name) + " your Cappuccino will be ready in about five minutes." + "\nThat'll be " + str(cptotal) + "$\n")
+    elif one_order == 3:
+        ftotal = FRESCO * number
+        print("\nOkay " + str(name) + " your Fresco will be ready in about five minutes." + "\nThat'll be " + str(ftotal) + "$\n")
+    elif one_order == 4:
+        cototal = COKTAIL * number
+        print("\nOkay " + str(name) + " your Coktail will be ready in about five minutes." + "\nThat'll be " + str(cototal) + "$\n")
+    else:
+        print("\nWe dont have that yet.\n")
     exit()
 
 def two_orders():
@@ -33,13 +58,7 @@ else:
     exit()
 
 
-"""COFFEE = 8
-
-CAPPUCCINO = 9
-
-FRESCO = 10
-
-COKTAIL = 11
+"""
 
 number = int (input("\nHow many of those will you take?\n"))
 
